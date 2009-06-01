8962a1e5719a534a0efbc31ce4017ebb424222f7
Andreas Kloeckner <inform@tiker.net>
2009-06-01 15:42:49 -0400

    Minor tweaks.

--- generate-timeline.sh
+++ generate-timeline.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
-./commit-timeline -d -o ~/tmp/timeline \
-  --timeline-js=$HOME/pack/simile-timeline/src/webapp/api/timeline-api.js \
+./commit-timeline -d -o ~/tmp/timeline --timezone=-5 \
   ~/dam/research/software/{hedge,pyrticle,codepy,pycuda,pymbolic,pytools,boostmpi,experiments,meshpy,pylo,pymetis,pyopencl,pyublas}
+scp -r ~/tmp/timeline/* tiker.net:public_html/git/commit-timeline
 
   
+  #--timeline-js=$HOME/pack/simile-timeline/src/webapp/api/timeline-api.js \
