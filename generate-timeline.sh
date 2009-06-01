8475f65bbcb889206fc0468550430580905ec895
Andreas Kloeckner <inform@tiker.net>
2009-05-31 23:07:37 -0400

    Actually working commit timeline code.

---
+++ generate-timeline.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+./commit-timeline -d -o ~/tmp/timeline \
+  --timeline-js=$HOME/pack/simile-timeline/src/webapp/api/timeline-api.js \
+  ~/dam/research/software/{hedge,pyrticle,codepy,pycuda,pymbolic,pytools,boostmpi,experiments,meshpy,pylo,pymetis,pyopencl,pyublas}
+
+  
